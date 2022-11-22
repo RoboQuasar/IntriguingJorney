bfb8f638ac056957c50b190ccec538920a9b34f2
RoboQuasar <roboquasar@gmail.com>
2022-11-22 16:17:55 +0300

    Реализация "The Very First Step"

--- src/scripts/Playground.ts
+++ src/scripts/Playground.ts
@@ -3,11 +3,7 @@ export class Playground {
       // This creates a basic Babylon Scene object (non-mesh)
       var scene = new BABYLON.Scene(engine);
 
-      // This creates and positions a free camera (non-mesh)
-      var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
-
-      // This targets the camera to scene origin
-      camera.setTarget(BABYLON.Vector3.Zero());
+      var camera = new BABYLON.ArcRotateCamera("camera", BABYLON.Tools.ToRadians(90), BABYLON.Tools.ToRadians(65), 10, BABYLON.Vector3.Zero(), scene);
 
       // This attaches the camera to the canvas
       camera.attachControl(canvas, true);
@@ -18,14 +14,32 @@ export class Playground {
       // Default intensity is 1. Let's dim the light a small amount
       light.intensity = 0.7;
 
-      // Our built-in 'sphere' shape. Params: name, options, scene
-      var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter: 2, segments: 32}, scene);
+      // // Our built-in 'sphere' shape. Params: name, options, scene
+      // var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter: 2, segments: 32}, scene);
 
-      // Move the sphere upward 1/2 its height
-      sphere.position.y = 1;
+      // // Move the sphere upward 1/2 its height
+      // sphere.position.y = 1;
 
       // Our built-in 'ground' shape. Params: name, options, scene
       var ground = BABYLON.MeshBuilder.CreateGround("ground", {width: 6, height: 6}, scene);
+      ground.position.y = -1;
+      let groundMaterial = new BABYLON.StandardMaterial("Ground Material", scene);
+
+      let groundTexture = new BABYLON.Texture("https://assets.babylonjs.com/textures/", scene);
+      groundMaterial.diffuseTexture = groundTexture;
+      
+      ground.material = groundMaterial;
+
+      const yetiMesh = BABYLON.SceneLoader.ImportMesh(
+        "",
+        "https://assets.babylonjs.com/meshes/Yeti/MayaExport/glTF/",
+        "Yeti.gltf",
+        scene,
+        function(newMeshes) {
+          newMeshes[0].scaling = new BABYLON.Vector3(0.1, 0.1, 0.1);
+          newMeshes[0].position.y = -1;
+        },
+      );
 
       return scene;
   }
