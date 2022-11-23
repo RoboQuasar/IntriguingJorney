04c4906bbc46c28caf58d94c60bbf04752f1e1b1
RoboQuasar <roboquasar@gmail.com>
2022-11-23 23:12:08 +0300

    Removed garbage and rewrite inde.ts

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,1 +1,16 @@
-export { Playground } from './Playground';
+import { Playground } from './Playground';
+
+const canvas = document.getElementById("renderCanvas") as HTMLCanvasElement; // Get the canvas element
+const engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engine
+const createScene = function () {
+  return Playground.CreateScene(engine, engine.getRenderingCanvas());
+};
+const scene = createScene(); //Call the createScene function
+// Register a render loop to repeatedly render the scene
+engine.runRenderLoop(function () {
+        scene.render();
+});
+// Watch for browser/canvas resize events
+window.addEventListener("resize", function () {
+        engine.resize();
+});
